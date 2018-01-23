72965d602bbbd2b822dd81f87cc657b8c6f29a88
DovileP <dovilepreidyte@gmail.com>
2018-01-23 21:20:55 +0200

    Add files via upload

---
+++ parametrai.py
@@ -0,0 +1,29 @@
+# Input file name for hmm profile that will be made into alignments
+
+input_file = "nifBall.fasta"
+
+# Software to use for the alignment: clustalw or muscle
+
+type = "muscle"
+
+# Output file name for the alignment file
+
+output_file  = "algm.aln"
+
+# Name of fully made hmm file
+
+hmm_file = 'nifbam.hmm'
+
+# Name of genome database that will be made into protein database
+
+database = "viruses.fasta"
+
+# Name of the new tadabase
+
+new_database = "viruses_translate.fasta"
+
+# Number of frames for translation
+
+frames = "6"
+
+
